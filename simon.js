4b86da426828f3464127d5170eba57e3f243f502
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-05-13 00:57:42 -0400

    Displays four buttons with label in middle

--- simon.js
+++ simon.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     blackBaseHeightEqualsWidth();
+    resizeButtons();
     var initialWindowHeight = $(window).height(),
         initialWindowWidth = $(window).width;
     $(window).resize(function () {
@@ -10,8 +11,10 @@ $(document).ready(function () {
         } else if (currentWindowHeight != initialWindowHeight) { // height changed
             blackBaseWidthEqualsHeight();
         }
+        resizeButtons();
     });
 });
+
 function blackBaseHeightEqualsWidth() {
     var blackBase = $('#black-base');
     var blackCircleWidth = blackBase.width();
@@ -24,3 +27,12 @@ function blackBaseWidthEqualsHeight() {
     blackBase.width(blackCircleHeight);
 }
 
+function resizeButtons() {
+    var buttonRow = $('.button-row'),
+        rowHeight = buttonRow.height(),
+        rowWidth = buttonRow.width();
+    var buttons = $('.button'),
+        buttonWidth = buttons.parent();
+    buttons.height(rowHeight * .8);
+    buttons.width(rowWidth * .5 * .8);
+}
